3faafd149239b38d2da40b60c5c479dcc25f6839
fahmida-faiza <fahmidafaiza57@gmail.com>
2025-04-15 20:39:03 +0600

    added interface on object contract

---
+++ TSCOM/src/index.ts
@@ -0,0 +1,13 @@
+const getUsername = document.querySelector("#user") as HTMLInputElement;
+const formSubmit = document.querySelector(".form") as HTMLFormElement;
+const main_container = document.querySelector(".main-container") as HTMLElement;
+
+
+// 
+interface UserData{
+    id:number;
+    login:string;
+    avatar_url:string;
+    location:string;
+    url:string;
+}
