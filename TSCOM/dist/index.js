671d80f63119485c6264042a89237eed88f892a0
fahmida-faiza <fahmidafaiza57@gmail.com>
2025-04-18 21:35:52 +0600

    added show the UI

--- TSCOM/dist/index.js
+++ TSCOM/dist/index.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log("hey whatsup, bye");
+const getUsername = document.querySelector("#user");
+const formSubmit = document.querySelector(".form");
+const main_container = document.querySelector(".main-container");
+// resuable function
+async function myCustomFetcher(url, options) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(`Network not ok -status : ${response.status}`);
+    }
+    const data = response.json();
+    console.log(data);
+    return data;
+}
+// let display the card UI
+const showResultUI = (singleUser) => {
+    const { avatar_url, login, url } = singleUser;
+    main_container.insertAdjacentHTML("beforeend", `<div class='card'>
+        <img src=${avatar_url} alt=${login}/>
+        <hr/>
+        <div class="card-footer">
+        <img src="${avatar_url}" alt="${login}"/>
+        <a href= "${url}">Github</a>
+        </div>
+        </div>
+        `);
+};
+function fetchUserData(url) {
+    myCustomFetcher(url, {}).then((userInfo) => {
+        for (const singleUser of userInfo) {
+            showResultUI(singleUser);
+            console.log("login " + singleUser.login);
+        }
+    });
+}
+// default fun call page load
+fetchUserData("https://api.github.com/users");
--- TSCOM/src/index.ts
+++ TSCOM/src/index.ts
@@ -12,7 +12,54 @@ interface UserData{
     url:string;
 }
 
+
+// resuable function
+async function  myCustomFetcher <T>(url:string , options?: RequestInit):
+Promise<T> {
+
+const response = await fetch (url, options)
+
+ if(!response.ok) {
+    throw new Error(
+        `Network not ok -status : ${response.status}`
+    )
+ }
+ const data = response.json()
+ console.log(data)
+ return data;
+ 
+}
+
+
+// let display the card UI
+const showResultUI = (singleUser: UserData)=>{
+
+    const {avatar_url,login,url}= singleUser;
+    main_container.insertAdjacentHTML(
+        "beforeend",
+        `<div class='card'>
+        <img src=${avatar_url} alt=${login}/>
+        <hr/>
+        <div class="card-footer">
+        <img src="${avatar_url}" alt="${login}"/>
+        <a href= "${url}">Github</a>
+        </div>
+        </div>
+        `
+    )
+}
+
 function fetchUserData(url: string){
+    
+    myCustomFetcher<UserData[]>(url, {}).then((userInfo) =>{
+
+        for(const singleUser of userInfo){
+            showResultUI(singleUser);
+            console.log("login " +singleUser.login)
+        }
+
+    })
+
     
 
 }
