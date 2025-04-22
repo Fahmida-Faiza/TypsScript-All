9503ac6f7c1c8724698bf7fb90eeab2cc20e1f7a
fahmida-faiza <fahmidafaiza57@gmail.com>
2025-04-23 00:49:03 +0600

    added construction

--- code all basic/class.js
+++ code all basic/class.js
@@ -1,10 +1,21 @@
+// blueprint of similar data use
+// class Persons {
+//     name:string= 'kokila';
+//     age:number=12;
+//     hobbies: string[] = ["reading", "singing"];
+// }
+// const persons1: Persons= new Persons() ;
+// console.log( persons1.hobbies);
 var Persons = /** @class */ (function () {
-    function Persons() {
-        this.name = 'kokila';
-        this.age = 12;
-        this.hobbies = ["reading", "singing"];
+    function Persons(name, age, hobbies) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
     }
     return Persons;
 }());
-var persons1 = new Persons();
+var persons1 = new Persons("vinod", 12, ["reading", "writing"]);
+// const persons1: Persons = new Persons(name : "vinod", age : 12, hobbies: ["reading", "writing"]);
+// const persons2: Persons = new Persons(name : "thapa", age : 15, hobbies: ["coding", "writing"]);
+// const persons3: Persons = new Persons(name : "bhar", age : 24, hobbies: ["sports", "writing"]);
 console.log(persons1);
--- code all basic/class.ts
+++ code all basic/class.ts
@@ -1,9 +1,37 @@
-class Persons {
-    name:string= 'kokila';
-    age:number=12;
-    hobbies: string[] = ["reading", "singing"];
+// blueprint of similar data use
 
+
+
+// class Persons {
+//     name:string= 'kokila';
+//     age:number=12;
+//     hobbies: string[] = ["reading", "singing"];
+
+// }
+
+// const persons1: Persons= new Persons() ;
+
+// console.log( persons1.hobbies);
+
+
+class Persons {
+    name: string;
+    age: number;
+    hobbies: string[];
+
+
+
+    constructor(name: string, age: number, hobbies: string[]) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
 }
 
-const persons1: Persons= new Persons() ;
-console.log( persons1);
+const persons1: Persons = new Persons("vinod", 12, ["reading", "writing"]);
+
+// const persons1: Persons = new Persons(name : "vinod", age : 12, hobbies: ["reading", "writing"]);
+// const persons2: Persons = new Persons(name : "thapa", age : 15, hobbies: ["coding", "writing"]);
+// const persons3: Persons = new Persons(name : "bhar", age : 24, hobbies: ["sports", "writing"]);
+
+console.log(persons1)
